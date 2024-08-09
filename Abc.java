0062da6f6674fbbec16c7997942f7ee8e7031a12
arjunasn476 <arjuna_notanubun_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:29:58 +0700

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,6 @@
+public class ABC {
+    public static void main (String[] args) {
+        System.out.println("nama saya arjuna soedewo notanubun dari kelas 10 rpl 7 yang berasal dari kota blitar");
+        String a = "1";
+    }
+}
---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("nama saya arjuna soedewo notanubun dari kelas 10 rpl 7 yang berasal dari blitar");
+        String a = "1";
+    }
+}
